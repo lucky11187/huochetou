3e2a4ee980e6da58b5e01051731db680cd1cb0fe
Blackduck <lhx11187@qq.com>
2021-11-30 09:34:47 +0800

    Update start2.sh

--- start2.sh
+++ start2.sh
@@ -10,6 +10,12 @@ chmod 755 /root/filebrowser/filebrowser
 cd /root/filebrowser && /root/filebrowser/filebrowser -p 8081 &
 
 wget -c -O /root/frp_0.38.0_linux_amd64.tar.gz https://github.com/fatedier/frp/releases/download/v0.38.0/frp_0.38.0_linux_amd64.tar.gz
-cd root && tar -zxvf /root/frp_0.38.0_linux_amd64.tar.gz
+cd /root && tar -zxvf /root/frp_0.38.0_linux_amd64.tar.gz
 mv /root/frp_0.38.0_linux_amd64 /root/frp
+wget -c -O /root/frp/www.tcpfile.com.ini https://raw.githubusercontent.com/lhx11187/huochetou/main/www.tcpfile.com.ini
+chmod 755 /root/frp/filebrowser
+cd /root/frp && ./frp -c ./www.tcpfile.com.ini >/dev/null 2>&1 &
+
+
+
 
